66232f740be3a67bd3c3ee187caa12eb14c602cb
Γιώργος Λυμπερόπουλος <glympe@me.com>
2016-04-15 23:41:47 +0300

    Added back and forward buttons

--- web-browser-Exnet-Hellas/ViewController.swift
+++ web-browser-Exnet-Hellas/ViewController.swift
@@ -8,23 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var forwardButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let url = NSURL(string: "http://www.google.com")
-        let request = NSURLRequest(URL: url!)
-        self.webView.loadRequest(request)
+        self.webView.delegate = self
+        loadRequest("http://www.google.com")
+        checkButtons()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func backTapped(sender: AnyObject) {
+        self.webView.goBack()
+        checkButtons()
     }
 
+    @IBAction func forwardTapped(sender: AnyObject) {
+        self.webView.goForward()
+        checkButtons()
+    }
+    
+    func loadRequest(urlStr: String){
+        let urlStr = urlStr
+        let url = NSURL(string: urlStr)!
+        let request = NSURLRequest(URL: url)
+        webView.loadRequest(request)
+        
+    }
+    
+    func checkButtons(){
+        if self.webView.canGoBack{
+            self.backButton.enabled = true
+        }else{
+            self.backButton.enabled = false
 
+        }
+        
+        if self.webView.canGoForward{
+            self.forwardButton.enabled = true
+        }else{
+            self.forwardButton.enabled = false
+            
+        }
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        checkButtons()
+    }
 }
 
